2afc0f3b83e0cd3753b68333085cd326be10130d
Gorbonosova <gorbonosova@gmail.com>
2024-05-16 15:41:13 +0300

    2

--- ls.js
+++ ls.js
@@ -5,16 +5,43 @@ function setLS(info) {
 }
 
 function getLS() {
-    const likesArray = JSON.parse(localStorage(likesKey));
-    if (!likesArray) {
+    const likesArray = JSON.parse(localStorage.getItem(likesKey));
+    
+    if (likesArray === null) {
         return [];
     }
     return likesArray;
 }
 
-function findPhotoId(photoId) {
-    likesArray = getLS();
-    return likesArray.find((likesPhotoId) => {photoId === likesPhotoId})
+function savePhotoIdLS(photoId) {
+    const likesArray = getLS();
+    likesArray.push(photoId);
+    setLS(likesArray);
 }
 
-export { getLS, setLS, findPhotoId };
+function includesPhotoId(photoId) {
+    const likesArray = getLS();
+    return likesArray.includes(photoId);
+}
+
+function findPhotoId(photoId) {
+    const likesArray = getLS();
+    return likesArray.find((likesPhotoId) => photoId === likesPhotoId)
+}
+
+function removeLikeLS(photoId) {
+    const likesArray = getLS();
+    if (includesPhotoId(photoId)) {
+        const dislikePhotoId = findPhotoId(photoId);
+        likesArray.splice(likesArray.indexOf(dislikePhotoId), 1);
+        setLS(likesArray);
+    }
+}
+function saveLikesPhoto(photoId) {
+    const likesArray = getLS();
+    likesArray.push(photoId);
+    setLS(likesArray);
+}
+
+
+export { setLS, getLS, savePhotoIdLS, includesPhotoId, findPhotoId, removeLikeLS, saveLikesPhoto };
--- script.js
+++ script.js
@@ -1,16 +1,13 @@
-//import { setLS, findPhotoId } from "./ls.js";
+import { setLS, getLS, savePhotoIdLS, includesPhotoId, findPhotoId, removeLikeLS, saveLikesPhoto } from "./ls.js";
 
 const APIKey = 'Nv6dAHUe_4Z37VzYuS_mTYr2ciTSRUIySR_7Yfbjumo';
 let isFetching = false;
 let page = 1;
 const iconLike = `<?xml version="1.0" encoding="UTF-8"?>
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20"><g id="_01_align_center" data-name="01 align center"><path d="M17.5.917a6.4,6.4,0,0,0-5.5,3.3A6.4,6.4,0,0,0,6.5.917,6.8,6.8,0,0,0,0,7.967c0,6.775,10.956,14.6,11.422,14.932l.578.409.578-.409C13.044,22.569,24,14.742,24,7.967A6.8,6.8,0,0,0,17.5.917ZM12,20.846c-3.253-2.43-10-8.4-10-12.879a4.8,4.8,0,0,1,4.5-5.05A4.8,4.8,0,0,1,11,7.967h2a4.8,4.8,0,0,1,4.5-5.05A4.8,4.8,0,0,1,22,7.967C22,12.448,15.253,18.416,12,20.846Z"/></g></svg>
-`;
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20"><g id="_01_align_center" data-name="01 align center"><path d="M17.5.917a6.4,6.4,0,0,0-5.5,3.3A6.4,6.4,0,0,0,6.5.917,6.8,6.8,0,0,0,0,7.967c0,6.775,10.956,14.6,11.422,14.932l.578.409.578-.409C13.044,22.569,24,14.742,24,7.967A6.8,6.8,0,0,0,17.5.917ZM12,20.846c-3.253-2.43-10-8.4-10-12.879a4.8,4.8,0,0,1,4.5-5.05A4.8,4.8,0,0,1,11,7.967h2a4.8,4.8,0,0,1,4.5-5.05A4.8,4.8,0,0,1,22,7.967C22,12.448,15.253,18.416,12,20.846Z"/></g></svg>`;
 const iconLikeChoose = `<?xml version="1.0" encoding="UTF-8"?>
-<svg xmlns="http://www.w3.org/2000/svg" id="Layer_1" data-name="Layer 1" viewBox="0 0 24 24" width="20" height="20"><path d="M17.5.917a6.4,6.4,0,0,0-5.5,3.3A6.4,6.4,0,0,0,6.5.917,6.8,6.8,0,0,0,0,7.967c0,6.775,10.956,14.6,11.422,14.932l.578.409.578-.409C13.044,22.569,24,14.742,24,7.967A6.8,6.8,0,0,0,17.5.917Z"/></svg>
-`;
+<svg xmlns="http://www.w3.org/2000/svg" id="Layer_1" data-name="Layer 1" viewBox="0 0 24 24" width="20" height="20"><path d="M17.5.917a6.4,6.4,0,0,0-5.5,3.3A6.4,6.4,0,0,0,6.5.917,6.8,6.8,0,0,0,0,7.967c0,6.775,10.956,14.6,11.422,14.932l.578.409.578-.409C13.044,22.569,24,14.742,24,7.967A6.8,6.8,0,0,0,17.5.917Z"/></svg>`;
 const imageContEl = document.querySelector('#photo-container');
-
 
 async function getImages(photo) {
     isFetching = true;
@@ -44,17 +41,15 @@ async function loadPhoto(partOfLink) {
     } catch (error) {
         alert(error);
     }
-    
 };
-
 
 function renderImages(image) {
     imageContEl.innerHTML = '';
     imageContEl.insertAdjacentHTML('beforeend', `<img src="${image.urls.regular}" class="photo" alt="${image.alt_description}"></img>`);
     const likesCounter = image.likes;
     
-    if (findPhotoId(image.id) === null) {
-        fillPhotoBottom(image.user.name, iconLikeChoose, likesCounter++);
+    if (includesPhotoId(image.id)) {
+        fillPhotoBottom(image.user.name, iconLikeChoose, likesCounter + 1);
     } else {
         fillPhotoBottom(image.user.name, iconLike, likesCounter);
     }
@@ -63,23 +58,21 @@ function renderImages(image) {
     likesEl.addEventListener('click', () => {
         const likeElement = document.querySelector('.like');
         const photoId = image.id;
+        const url = image.urls.regular;
+        const des = image.alt_description;
+
         if (!includesPhotoId(photoId)) {
             likeElement.innerHTML = `<div class="like">${iconLikeChoose} ${likesCounter + 1}</div>`;
             saveLikesPhoto(photoId) 
-            createPhotoHistoryEl(photoId, image.url.regular, image.alt_description)
+            createPhotoHistoryEl(photoId, url, des)
         } else {
             likeElement.innerHTML = `<div class="like">${iconLike} ${likesCounter}</div>`;
             removeLikeLS(photoId);
+            removePhotoHistoryEl(photoId);
         }
-        
-
     });
-    
 }
 
-function savePhotoInHistory(params) {
-    
-}
 function getHistoryTitle() {
     const historyEl = document.createElement('div');
     historyEl.classList.add('history');
@@ -88,7 +81,6 @@ function getHistoryTitle() {
     const historyBoxEl = document.createElement('div');
     historyBoxEl.classList.add('history-box');
     historyEl.append(historyBoxEl);
-    
 }
 
 async function processArray(array) {
@@ -99,7 +91,6 @@ async function processArray(array) {
     historyBoxEl.addEventListener('click', async ({target}) => {
         const photoEl = target;
         const photoId = photoEl.dataset.id;
-        const newPhoto = await getImages(photoId);
         loadPhoto(photoId);
     });
   }
@@ -111,7 +102,6 @@ async function getPhotoEl(photoId) {
     } catch (error) {
         console.log(error);
     }
-    
 }
 
 function createPhotoHistoryEl(phototId, urls, descripton) {
@@ -119,72 +109,15 @@ function createPhotoHistoryEl(phototId, urls, descripton) {
         historyBoxEl.insertAdjacentHTML('beforeend', `<img class="history-photo" data-id="${phototId}" src="${urls}" alt="${descripton}"></img>` )
 }
 
+function removePhotoHistoryEl(photoId) {
+    const removeEl = document.querySelector(`[data-id="${photoId}"]`);
+    console.log(removeEl + 'remove');
+    removeEl.remove();
+}
+
 function fillPhotoBottom(author, icon, likesCounter) {
     imageContEl.insertAdjacentHTML('beforeend', `<div class="bottom-photo"><h3 class="author">${author}</h3><div class="like">${icon} ${likesCounter}</div></div>`) 
 }
 
 
-
-// LocalStorage
-const likesKey = 'likes';
-
-function setLS(info) {
-    localStorage.setItem(likesKey, JSON.stringify(info))
-}
-
-function getLS() {
-    const likesArray = JSON.parse(localStorage.getItem(likesKey));
-    
-    if (likesArray === null) {
-        return [];
-    }
-    return likesArray;
-}
-
-function savePhotoIdLS(photoId) {
-    const likesArray = getLS();
-    likesArray.push(photoId);
-    setLS(likesArray);
-}
-
-function includesPhotoId(photoId) {
-    const likesArray = getLS();
-    return likesArray.includes(photoId);
-}
-
-function findPhotoId(photoId) {
-    const likesArray = getLS();
-    return likesArray.find((likesPhotoId) => {photoId === likesPhotoId})
-}
-
-function removeLikeLS(photoId) {
-    const likesArray = getLS();
-    if (includesPhotoId(photoId)) {
-        const dislikePhotoId = findPhotoId(photoId);
-        likesArray.splice(likesArray.indexOf(dislikePhotoId), 1);
-        setLS(likesArray);
-    }
-}
-function saveLikesPhoto(photoId) {
-    const likesArray = getLS();
-    likesArray.push(photoId);
-    setLS(likesArray);
-}
-
-
-/* window.addEventListener('scroll', async () => {
-    const scrollHeight = Math.max(
-        document.body.scrollHeight, document.documentElement.scrollHeight,
-        document.body.offsetHeight, document.documentElement.offsetHeight,
-        document.body.clientHeight, document.documentElement.clientHeight
-      );
-      if(this.window.scrollY >= scrollHeight * 0.75 && !isFetching) {
-        try {
-            renderImages(await getImages(page++));
-        } catch (error) {
-            alert(error)
-        }
-        
-      };
-}); */
 
